916d4d05639976631b7df70c9876eb019796f4ac
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2019-09-10 16:50:45 +0530

    task create

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,23 +5,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String,
-//         trim:true,
-//         required:true
-//     },
-//     completed: {
-//         type: Boolean,
-//         default:false
-//     }
-// })
-// const laundry = new Task({
-//     description: 'laundry',
-//     completed: false
-// })
-// laundry.save().then(() => {
-//     console.log(laundry)
-// }).catch((error) => {
-//     console.log(error)
-// })
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
@@ -8,6 +9,14 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
         res.send(user)
+    }).catch(() => {
+
+    })
+})
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.send(task)
     }).catch(() => {
 
     })
