58848cb9f55a6083ecc2939912bce580b67d8361
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2019-09-10 13:55:52 +0530

    validators

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,22 +1,47 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
-    useNewUrlParser:true,
-    useCreateIndex:true
+const validator = require('validator')
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
- const User = mongoose.model('User',{
-     name:{
-         type:String
-     },
-     age:{
-         type:Number
-     }
- })
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required:true
+        
+    },
+    age: {
+        type: Number,
+        validate(value){
+            if(value<0){
+                throw new Error('age cant be negetive')
+            }
+        }
+    }
+})
  const me =new User({
      name:'Raunak',
-     age:20
+     age:-20
  })
  me.save().then(()=>{
      console.log(me)
  }).catch((error)=>{
      console.log(error)
  })
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+// const laundry = new Task({
+//     description: 'laundry',
+//     completed: false
+// })
+// laundry.save().then(() => {
+//     console.log(laundry)
+// }).catch((error) => {
+//     console.log(error)
+// })
