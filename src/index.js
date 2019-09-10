a84100e4acd14d6e6567536c367a70ee0abdb6d9
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2019-09-10 16:55:32 +0530

    status added

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ app.use(express.json())
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
-        res.send(user)
-    }).catch(() => {
-
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
     })
 })
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
-        res.send(task)
-    }).catch(() => {
-
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
     })
 })
 app.listen(port, () => {
